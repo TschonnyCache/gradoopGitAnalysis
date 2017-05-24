193a86e7648de692d5b43335d760cc2846da3d94
Jonas Kreusch <krejon@gmail.com>
2017-05-24 11:46:32 +0200

    implemented methods for creating vertices from branch and commit

--- src/main/java/gradoopify/GradoopFiller.java
+++ src/main/java/gradoopify/GradoopFiller.java
@@ -1,10 +1,14 @@
 package gradoopify;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.flink.api.common.ProgramDescription;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand.ListMode;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.errors.AmbiguousObjectException;
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.RevisionSyntaxException;
@@ -22,6 +26,8 @@ import io.LoadJGit;
 
 public class GradoopFiller implements ProgramDescription {
 	public static final String userVertexLabel = "user";
+	public static final String branchVertexLabel = "branch";
+	public static final String commitVertexLabel = "commit";
 	private GradoopFlinkConfig config;
 
 	public GradoopFiller(GradoopFlinkConfig config) {
@@ -37,6 +43,20 @@ public class GradoopFiller implements ProgramDescription {
 		props.set("timezoneOffset", user.getTimeZoneOffset());
 		return config.getVertexFactory().createVertex(userVertexLabel, props);
 	}
+	
+	public Vertex createVertexFromBranch(Ref branch) {
+		Properties props = new Properties();
+		props.set("name",branch.getName());
+		return config.getVertexFactory().createVertex(branchVertexLabel,props);
+	} 
+	
+	public Vertex createVertexFromCommit(RevCommit commit) {
+		Properties props = new Properties();
+		props.set("name",commit.name() );
+		props.set("time",commit.getCommitTime());
+		props.set("message",commit.getFullMessage());
+		return config.getVertexFactory().createVertex(commitVertexLabel,props);
+	}
 
 	public static void main(String[] args) {
 
@@ -44,15 +64,26 @@ public class GradoopFiller implements ProgramDescription {
 		GradoopFlinkConfig gradoopConf = GradoopFlinkConfig.createConfig(env);
 		GradoopFiller gf = new GradoopFiller(gradoopConf);
 		LoadJGit ljg = new LoadJGit();
-		Repository repository = ljg.openRepo("/home/ohdorno/git/spark");
+		Repository repository = ljg.openRepo("/Users/jonas/Documents/Code/spark");
 		Map<String, Ref> mapRefs = ljg.getAllHeadsFromRepo(repository);
+		try {
+			List<Ref> branchs = new Git(repository).branchList().setListMode( ListMode.ALL ).call();
+			for(Ref branch:branchs){
+				Vertex v = gf.createVertexFromBranch(branch);
+			}
+		} catch (GitAPIException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 
 		try (RevWalk revWalk = new RevWalk(repository)) {
 			ObjectId commitId;
 			commitId = repository.resolve("refs/heads/master");
 			revWalk.markStart(revWalk.parseCommit(commitId));
-			Vertex v = gf.createVertexFromUser(revWalk.next().getAuthorIdent());
-			System.out.println(v);
+			Vertex vc = gf.createVertexFromCommit(revWalk.next());
+			Vertex vu = gf.createVertexFromUser(revWalk.next().getAuthorIdent());
+			System.out.println(vu);
 		} catch (RevisionSyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
